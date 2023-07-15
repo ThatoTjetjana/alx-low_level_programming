64d9c3e73fdb4fe848071736aa1bfa8db4e42146
ThatoTjetjana <thato.tjetjana@gmail.com>
2023-07-14 22:24:02 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,10 @@
 /**
  * main - print a string using printf
  *
- * Return: Always 0
+ * Return: 0
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
+}
