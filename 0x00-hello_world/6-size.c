34945e9f662dc2323841f7976616d3b1c6768b6f
ThatoTjetjana <thato.tjetjana@gmail.com>
2023-07-14 22:46:16 -0700

    Size types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,3 +17,4 @@ printf("Size of a long int: %lu byte(s)\n , sizeof(l));
 printf("Size of a long long int: %lu byte(s)\n , sizeof(ll));
 printf("Size of a float: %lu byte(s)\n , sizeof(f));
 return (0);
+}
